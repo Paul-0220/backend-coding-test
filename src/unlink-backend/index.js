825b27be4918e0e2fe4ceaac79e7710fa603dbdf
Prashannjeet Paul <prashannjeet02@gmail.com>
2023-04-24 17:43:15 +0530

    index file updates

--- src/unlink-backend/index.js
+++ src/unlink-backend/index.js
@@ -1,14 +1,23 @@
+/* eslint-disable no-undef */
 "use strict";
 
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const route = require("./route/route");
 
-//* Route testing...
-app.get("/test", (req, res) => {
-  res.send("Testing the route");
-});
+// Body-parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// *middleware
+app.use("/", route);
+
+// //* Route testing...
+// app.get("/health", (req, res) => {
+//   res.send("Testing the route");
+// });
 
 //*Database Connection.....
 mongoose
@@ -24,6 +33,6 @@ mongoose
   });
 
 //* Server..
-app.listen(3000, () => {
-  console.log("server running successfully at Port:3000");
+app.listen(8010, () => {
+  console.log("server running successfully at Port:8010");
 });
