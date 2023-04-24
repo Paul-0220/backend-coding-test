3ee6bff5b1100a28e7b8d1675ac94c41b3ae706c
Prashannjeet Paul <prashannjeet02@gmail.com>
2023-04-24 17:44:29 +0530

    eslint configs

---
+++ src/unlink-backend/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: ["eslint:recommended"],
+};
+
+// module.exports = {
+//   parserOptions: {
+//     ecmaVersion: "Latest",
+//   },
+//   env: {
+//     es6: true,
+//   },
+//   extends: ["eslint:recommended"],
+// };
